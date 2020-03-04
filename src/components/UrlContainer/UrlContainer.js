5a55101f6fb3a20d79dd366f6af793a0f7b03d0d
Andrew Kinstler <andrewckinstler@gmail.com>
2020-03-04 09:43:44 -0700

    Refactor App to no longer be a Class based component

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './UrlContainer.css';
+import { setUrls } from '../../actions';
+import { getUrls } from '../../apiCalls';
+import { connect } from 'react-redux';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+export class UrlContainer extends Component {
+  constructor() {
+    super() 
+  }
+
+  componentDidMount() {
+    getUrls()
+      .then(data => this.props.setUrls(data.urls))
+      .catch(err => console.error('Error fetching:', err));
+  };
+
+  render() {
+    const urlEls = this.props.urls.map(url => {
+      return (
+        <div className="url">
+          <h3>{url.title}</h3>
+          <a href={url.short_url} target="blank">{url.short_url}</a>
+          <p>{url.long_url}</p>
+        </div>
+      )
+    })
+    
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
+      <section>
+        { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
+      </section>
     )
-  });
-
-  return (
-    <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
-    </section>
-  )
+  }
 }
 
-export default UrlContainer;
+export const mapStateToProps = ({ urls }) => ({
+  urls
+});
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    setUrls: urls => dispatch(setUrls(urls))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UrlContainer);
